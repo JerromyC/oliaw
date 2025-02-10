dc7998568a1fa22b7be3a6786709238b458ef94f
Jerromy Chance <jerromy.chance31191@gmail.com>
2025-02-10 15:41:03 -0600

    Sorted styles out into different sheets - fixed an issue with tile width, but will need to be revisited

---
+++ product.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("dummy-products.json")
+        .then(response => response.json())
+        .then(products => {
+            const productWrapper = document.querySelector(".product-wrapper");
+            productWrapper.innerHTML = ""; // Clear existing static HTML
+
+            products.forEach(product => {
+                const productHTML = `
+                    <div class="tile product">
+                        <div class="action-wrapper">
+                            <img src="./assets/star.gif" title="favorite" alt="Favorite">
+                            <img src="./assets/cart.gif" title="add to cart" alt="Add to cart">
+                        </div>
+                        <img src="${product.images[0]}" alt="${product.title}">
+                        <span>$${product.price.toFixed(2)}</span>
+                        <p>${product.title}</p>
+                        <a href="#" class="button small-button">View Details</a>
+                    </div>
+                `;
+                productWrapper.innerHTML += productHTML;
+            });
+        })
+        .catch(error => console.error("Error fetching products:", error));
+});
